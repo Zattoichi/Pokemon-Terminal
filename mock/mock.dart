fa7b779e9e60be8585b834f8969a19db00a2e812
Joao Pedro <laharlx@hotmail.com>
2020-01-06 12:11:17 -0300

    Adicionei um pokemon

--- mock/mock.dart
+++ mock/mock.dart
@@ -2,8 +2,8 @@ import '../models/pokemons.dart';
 
 class Mock{
   List<Pokemon> pokemonsList = [
-    new Pokemon("Jatinho ", 2, ["Jato d'agua"]),
+    new Pokemon("ivysaur ", 2, ["chlorophyll","overgrow",]),
     new Pokemon(nome, pokedexIndex, abilities),
-    
+
   ];
 }
