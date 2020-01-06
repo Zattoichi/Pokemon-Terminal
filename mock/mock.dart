aefa55c1d2200445af03f7ae88c3374176074090
Joao Pedro <laharlx@hotmail.com>
2020-01-06 12:51:59 -0300

    9 Pokemons adicionados

--- mock/mock.dart
+++ mock/mock.dart
@@ -2,8 +2,16 @@ import '../models/pokemons.dart';
 //Instanciar é criar uma classe de um objeto
 class Mock{
   List<Pokemon> pokemonsList = [
-    new Pokemon("ivysaur ", 2, ["chlorophyll","overgrow",]),
-    new Pokemon(nome, pokedexIndex, abilities),
+    new Pokemon("Ivysaur ", 2, ["Chlorophyll","Overgrow",]),
+    new Pokemon("Charmander", 4, ["Solar-power","Blaze"]),
+    new Pokemon("Squirtle", 7, ["Rain-dish","Torrent"]),
+    new Pokemon("Caterpie", 10, ["Run-away","Shield-dust"]),
+    new Pokemon("Kakuna", 14, ["Shed-skin"]),
+    new Pokemon("Pidgey", 16, ["Big-pecks","Tangled-feet","Keen-eye"]),
+    new Pokemon("Raticate", 20, ["Hustle","Guts","Run-away"]),
+    new Pokemon("Arbok", 24, ["Unnerve","Shed-skin","Intimidate"]),
+    new Pokemon("Pikachu", 25, ["Lightning-rod","Static"]),
+    new Pokemon("Sandshrew", 27, ["Sand-rush","Sand-veil"]),
 
   ];
 }
