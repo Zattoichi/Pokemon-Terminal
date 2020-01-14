9bb334324a8f24e652a46c0bd4ca172de08dc1a9
Joao Pedro <laharlx@hotmail.com>
2020-01-14 15:36:50 -0300

    Criei a função fetchApi

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
 void main(List<String> args) {
 
-
+print()
 }
--- services/api.dart
+++ services/api.dart
@@ -5,10 +5,12 @@ class APIService{
   final apiUrl='https://pokeapi.co/api/v2/pokemon';
   Client client = Client();
 
-//'pokemonName' é para o facil entendimento na hora de chamar a função e preencher o parametro dado com os dados corretos; Nome de argumento 
-  void fetchApi(String pokemonName){
+  //'pokemonName' é para o facil entendimento na hora de chamar a função e preencher o parametro dado com os dados corretos; Nome de argumento 
+  void fetchApi(String pokemonName) async {
+    await client.get("$apiUrl/$pokemonName").then(onValue);
 
-
-  } 
+  }
 
 }
+  //Funções async; assíncrone não executa as funções em ordem, responde apenas quando os dados retornam
+  //Funções síncronas executam as funções na ordem, respondem mesmo sem os dados retornarem
