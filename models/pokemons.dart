d21a90e3e0955b96af7e139148a8089ac3d86ceb
Joao Pedro <laharlx@hotmail.com>
2020-01-14 14:04:41 -0300

    Deletei os metodos Attack e Evolve

--- models/pokemons.dart
+++ models/pokemons.dart
@@ -4,16 +4,6 @@ class Pokemon{
   int pokedexIndex;
   List abilities;
 
-
-  void attack(){
-    
-
-  }
-
-  void evolve(){
-
-
-  }
   //Construtor serve para poder usar o objeto em outros lugares 
   Pokemon(this.nome, this.pokedexIndex, this.abilities);
 
