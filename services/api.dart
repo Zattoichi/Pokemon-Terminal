6a6e16e15b14b61c47a46c0a66dac8e3a5642641
Joao Pedro <laharlx@hotmail.com>
2020-01-14 14:37:16 -0300

    Comecei a criar a classe APIService

---
+++ services/api.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' show Client;
+
+
+class APIService{
+  final apiUrl='https://pokeapi.co/api/v2/pokemon';
+  Client client = Client();
+
+//'pokemonName' é para o facil entendimento na hora de chamar a função e preencher o parametro dado com os dados corretos; Nome de argumento 
+  void fetchApi(String pokemonName){
+
+
+  } 
+
+}
