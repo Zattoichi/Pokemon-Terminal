19388df386c4d445fcddb7a29012ae80705417af
Joao Pedro <laharlx@hotmail.com>
2020-01-14 15:00:51 -0300

    Adicionei comentario

--- models/pokemons.dart
+++ models/pokemons.dart
@@ -8,6 +8,7 @@ class Pokemon{
   Pokemon({this.nome, this.pokedexIndex, this.abilities});
 
   // toString = descrição do objeto 
+  // $ <-- Pede o valor da variavel e coloca em uma string
   String toString() {
     return "Index: ${this.pokedexIndex}, Nome: ${this.nome}, Habilidades: ${this.abilities}";
   }
