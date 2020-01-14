b04e94e111765c702525645b59e2720f1209b920
Joao Pedro <laharlx@hotmail.com>
2020-01-14 14:11:21 -0300

    Refactor

--- main.dart
+++ main.dart
@@ -1,12 +1,4 @@
-import 'mock/mock.dart';
-import 'models/pokemons.dart';
-
 void main(List<String> args) {
-  Mock mock = new Mock();
 
-  List<Pokemon> lista = mock.pokemonsList;
-
-  print(lista.where((nomepokemon) => nomepokemon.nome == args[0]));
-  
 
 }
