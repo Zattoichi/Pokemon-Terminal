e48c1e7a12f3e398f42dfa464f5c67a41c12d051
Joao Pedro <laharlx@hotmail.com>
2020-01-14 15:55:36 -0300

    Finalizei a função fetchApi

--- services/api.dart
+++ services/api.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
 import 'package:http/http.dart' show Client;
-
-
-class APIService{
+import '../models/pokemons.dart';
+  //..
+  class APIService{
   final apiUrl='https://pokeapi.co/api/v2/pokemon';
   Client client = Client();
 
   //'pokemonName' é para o facil entendimento na hora de chamar a função e preencher o parametro dado com os dados corretos; Nome de argumento 
   void fetchApi(String pokemonName) async {
-    await client.get("$apiUrl/$pokemonName").then(onValue);
+    await client.get("$apiUrl/$pokemonName").then((dadosDoPokemon){
+      print(Pokemon.fromJson(jsonDecode(dadosDoPokemon.body)));
+    });
 
   }
 
